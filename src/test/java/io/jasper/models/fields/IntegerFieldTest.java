7099edec499ed32b5f7d32d7f346a476c1e46a51
asifmanan <asifmanan@gmail.com>
2024-03-14 20:16:58 +0000

    Unit tests on models.fields run successful, commiting test suit

--- src/test/java/io/jasper/models/fields/IntegerFieldTest.java
+++ src/test/java/io/jasper/models/fields/IntegerFieldTest.java
@@ -1,4 +1,28 @@
 package io.jasper.models.fields;
 
-public class IntegerField {
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegerFieldTest {
+    @Test
+    public void testInitialization(){
+        IntegerField field = new IntegerField();
+        assertEquals(Integer.valueOf(0), field.getValue(), "IntegerField should initialize with 0");
+    }
+
+    @Test
+    public void testSetValue(){
+        IntegerField field = new IntegerField();
+        int value = 10;
+        field.setValue(value);
+        assertEquals(Integer.valueOf(value), field.getValue(), "setValue should update the field's value");
+    }
+    @Test
+    public void testGetValue(){
+        IntegerField field = new IntegerField();
+        field.setValue(100);
+        assertEquals(Integer.valueOf(100),field.getValue(), "getValue should return the current value it holds");
+    }
 }
