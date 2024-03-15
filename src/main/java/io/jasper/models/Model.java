eaca774e9b84375b3c27430e5baf634d467810d4
asifmanan <asifmanan@gmail.com>
2024-03-15 02:01:04 +0000

    Fixed Models.java save() method

--- src/main/java/io/jasper/models/Model.java
+++ src/main/java/io/jasper/models/Model.java
@@ -1,7 +1,6 @@
 package io.jasper.models;
 
 import io.jasper.models.fields.IntegerField;
-import io.jasper.models.fields.PrimaryKey;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +9,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Model {
     private static final Map<Class<? extends Model>, Map<Integer, Model>> tables = new HashMap<>();
     private static final Map<Class<? extends Model>, AtomicInteger> idGenerators = new HashMap<>();
-    protected IntegerField id = new IntegerField();
+    protected IntegerField pk = new IntegerField();
     public Model(){
-        int idValue = generateIdForClass(getClass());
-        PrimaryKey pk = new PrimaryKey();
-        getTable().put(pk.getValue(),this);
+        int id = generateIdForClass(getClass());
+        this.pk.setValue(id);
+        getTable().put(id,this);
     }
     public static synchronized int generateIdForClass(Class<? extends Model> clazz){
         idGenerators.putIfAbsent(clazz, new AtomicInteger(0));
@@ -25,15 +24,18 @@ public class Model {
         return tables.get(this.getClass());
     }
     public void save(){
-        tables.putIfAbsent(this.getClass(),new HashMap<>());
+        getTable().put(this.pk.getValue(),this);
     }
     public void delete(){
-        if(id == null || id.getValue() == null){
+        if(pk == null || pk.getValue() == null){
             throw new IllegalStateException("Cannot delete a model without an ID");
         }
-        getTable().remove(id.getValue());
+        getTable().remove(pk.getValue());
     }
     public static <T extends Model> T findById(Class<T> clazz, int id){
         return clazz.cast(tables.getOrDefault(clazz, new HashMap<>()).get(id));
+    }
+    public int getId(){
+        return this.pk.getValue();
     }
 }
